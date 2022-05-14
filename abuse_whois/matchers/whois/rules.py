d58361cf6c55f5e6d11171f01a5f764caeb5432d
Manabu Niseki <manabu.niseki@gmail.com>
2022-05-14 12:04:45 +0900

    refactor: cache load_rules()

--- abuse_whois/matchers/whois/rules.py
+++ abuse_whois/matchers/whois/rules.py
@@ -1,4 +1,5 @@
 import pathlib
+from functools import lru_cache
 from typing import List
 
 from abuse_whois.utils import load_yaml
@@ -6,6 +7,7 @@ from abuse_whois.utils import load_yaml
 from .rule import WhoisRule
 
 
+@lru_cache(maxsize=1)
 def load_rules() -> List[WhoisRule]:
     rules: List[WhoisRule] = []
     for path in pathlib.Path(__file__).parent.glob("./rules/*.yaml"):
