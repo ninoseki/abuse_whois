d7658f1f9fa4a9e028ed7612c805c5dc4a8837bf
Manabu Niseki <manabu.niseki@gmail.com>
2022-06-25 08:41:53 +0900

    fix: use typing_extensions

--- abuse_whois/schemas/contact.py
+++ abuse_whois/schemas/contact.py
@@ -1,4 +1,9 @@
-from typing import Any, Literal, Optional
+from typing import Any, Optional
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
 
 from pydantic import Field
 
