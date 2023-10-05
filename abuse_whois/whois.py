1a4fcf72d94fa4ad412af488838b719c87bb019a
Manabu Niseki <manabu.niseki@gmail.com>
2023-10-05 21:11:52 +0900

    fix: catch ConnectionResetError

--- abuse_whois/whois.py
+++ abuse_whois/whois.py
@@ -1,8 +1,6 @@
 import asyncio
 
 import stamina
-from asyncache import cached
-from cachetools import TTLCache
 
 from . import errors, services, settings
 
@@ -12,7 +10,6 @@ from . import errors, services, settings
     attempts=settings.QUERY_MAX_RETRIES,
     timeout=None,
 )
-@cached(cache=TTLCache(maxsize=settings.QUERY_CACHE_SIZE, ttl=settings.QUERY_CACHE_TTL))
 async def query(
     hostname: str,
     *,
